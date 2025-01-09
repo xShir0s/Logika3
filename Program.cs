0a2ae1090e30b828b4488644e0622e43e7829cbb
xShir0s <mtodys2006@gmail.com>
2025-01-09 09:23:44 +0100

    dodanie plikow

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// first input 5
+// second input 3
+// output  "Liczba pierwsza jest większa od liczby drugiej"
+
+var value1 = Convert.ToDouble(Console.ReadLine());
+var value2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine(value2 / value1);
+
+
